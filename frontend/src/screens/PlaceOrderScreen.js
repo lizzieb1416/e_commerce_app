a18a806d2a12b5eeea07666e761190b3d6be8ca9
lisetTablet <lizethbastidas@gmail.com>
2021-04-09 13:46:25 +0200

    ni idea

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,35 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
 
-function PlaceOrderScreen() {
+function PlaceOrderScreen({ history }) {
+
+    const orderCreate = useSelector(state => state.orderCreate)
+    const { order, error, success } = orderCreate
+
+    const dispatch = useDispatch()
+
     const cart = useSelector(state => state.cart)
 
     cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
     cart.shippingPrice = (cart.itemsPrice > 100 ? 0 : 10).toFixed(2)
     cart.taxPrice = Number((0.082) * cart.itemsPrice).toFixed(2)
-    
+
     cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
 
+
+    if (!cart.paymentMethod) {
+        history.push('/payment')
+    }
+
+    useEffect(() => {
+        if (success) {
+            history.push(`/order/${order._id}`)
+            dispatch({ type: ORDER_CREATE_RESET })
+        }
+    }, [success, history])
+
     const placeOrder = () => {
-        console.log("Place order")
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+        }))
     }
 
     return (
         <div>
-            <CheckoutSteps step1 step2 step3 step4/>
+            <CheckoutSteps step1 step2 step3 step4 />
             <Row>
                 <Col md={8}>
-                    <ListGroup variant='flush'> 
+                    <ListGroup variant='flush'>
                         <ListGroup.Item>
                             <h2>Shipping</h2>
 
                             <p>
                                 <strong>Shipping: </strong>
-                                {cart.shippingAddress.address}, {cart.shippingAddress.city}
+                                {cart.shippingAddress.address},  {cart.shippingAddress.city}
                                 {'  '}
                                 {cart.shippingAddress.postalCode},
                                 {'  '}
@@ -39,9 +67,8 @@ function PlaceOrderScreen() {
 
                         <ListGroup.Item>
                             <h2>Payment Method</h2>
-
                             <p>
-                                <strong>Shipping: </strong>
+                                <strong>Method: </strong>
                                 {cart.paymentMethod}
                             </p>
                         </ListGroup.Item>
@@ -50,13 +77,13 @@ function PlaceOrderScreen() {
                             <h2>Order Items</h2>
                             {cart.cartItems.length === 0 ? <Message variant='info'>
                                 Your cart is empty
-                                </Message> : (
+                            </Message> : (
                                     <ListGroup variant='flush'>
                                         {cart.cartItems.map((item, index) => (
                                             <ListGroup.Item key={index}>
                                                 <Row>
                                                     <Col md={1}>
-                                                        <Image src={item.image} alt={item.name} fluid rounded/>
+                                                        <Image src={item.image} alt={item.name} fluid rounded />
                                                     </Col>
 
                                                     <Col>
@@ -72,7 +99,9 @@ function PlaceOrderScreen() {
                                     </ListGroup>
                                 )}
                         </ListGroup.Item>
+
                     </ListGroup>
+
                 </Col>
 
                 <Col md={4}>
@@ -95,7 +124,7 @@ function PlaceOrderScreen() {
                                     <Col>${cart.shippingPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
-                            
+
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Tax:</Col>
@@ -108,6 +137,11 @@ function PlaceOrderScreen() {
                                     <Col>Total:</Col>
                                     <Col>${cart.totalPrice}</Col>
                                 </Row>
+                            </ListGroup.Item>
+
+
+                            <ListGroup.Item>
+                                {error && <Message variant='danger'>{error}</Message>}
                             </ListGroup.Item>
 
                             <ListGroup.Item>
